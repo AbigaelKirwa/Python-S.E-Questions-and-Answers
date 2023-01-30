930ff062ab2502ab6964c6c07f125a2373023839
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2023-01-30 12:48:12 +0300

    added explicit and implicit conversion

---
+++ 27 explicit data type conversion.py
@@ -0,0 +1,10 @@
+# write a program to show explicit data type conversion
+# involves data conversion using methods like int() float() and string()
+
+str_num = "12"
+int_num = 15
+
+new_num = int(str_num) + int_num
+
+print("The sum of the two numbers is ", new_num)
+print("The datatype of the new number is ", type(new_num))
