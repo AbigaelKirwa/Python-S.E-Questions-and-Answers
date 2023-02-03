03059f1fdc7c024ffcbdc58e54c90d0c7c77c28c
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2023-02-03 16:43:08 +0300

    edited reverse string

--- 28 reverse string.py
+++ 28 reverse string.py
@@ -5,3 +5,20 @@ def strReverse(str_ent):
 
 str_ent = input("Write a string of your choice and have it reversed ")
 print ("The reversed string is ", strReverse(str_ent))
+
+
+
+"""
+def strReverse(str_ent):
+    reversed_str = ''
+    index = len(str_ent)
+
+    while index:
+        index -= 1
+        reversed_str += str_ent[index]
+
+    return reversed_str
+
+str_ent = input("Write a string of your choice and have it reversed ")
+print ("The reversed string is ", strReverse(str_ent))
+"""
