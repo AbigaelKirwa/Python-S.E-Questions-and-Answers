993869772d54b4eb74bd1828b8e8b8b746dddb5a
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2023-02-17 16:31:40 +0300

    ammended file

--- 30 find first non repeated character in string.py
+++ 30 find first non repeated character in string.py
@@ -1,14 +1,21 @@
 #this is a program to find the first non-repeated character in a string
 
 def first_non_repeated_character(str_ent):
+    index = 0
+    str = ""
 
     for i in str_ent:
         if str_ent.count(i) == 1:
+            str += i
             break
         else: 
+            index +=1
             continue
-    return i
+    if index >= len(str_ent):
+        print("Either all characters are repeating or string is empty")
+    else:
+        print("First non-repeating character is ", str)
 
 
 str_ent = input("Enter your string of choice ")
-print("The first non repeated character is ", first_non_repeated_character(str_ent))
+print(first_non_repeated_character(str_ent))
