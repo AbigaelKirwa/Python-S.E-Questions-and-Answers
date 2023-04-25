78529434fd387834d8a1870a9c4008022340b643
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2023-04-25 05:27:51 +0300

    corrected 20 extracting numbers from string

--- 20 extracting numbers from string.py
+++ 20 extracting numbers from string.py
@@ -1,7 +1,7 @@
 def extractnum(string_input):
 
     numbers = [int(i) for i in string_input.split() if i.isdigit()]
-    return str(numbers)
+    return (numbers)
 
 string_input = input("Enter a string ")
 print("The numbers in the string are :", extractnum(string_input))
