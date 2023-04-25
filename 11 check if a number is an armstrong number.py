1026f189c87c72b707173d9dae412414879e85ac
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2023-04-25 05:20:18 +0300

    fixed error in 11 armstrong numbers

--- 11 check if a number is an armstrong number.py
+++ 11 check if a number is an armstrong number.py
@@ -2,13 +2,11 @@
 
 num = int(input("Enter a number to check if it is an armstrong number "))
 
-order = len(str(num))
-
 sum = 0
 temp = num
 while (temp > 0):
     digit = temp % 10
-    sum = sum + (digit ** order)
+    sum = sum + (digit ** 3)
     temp = temp // 10
 
 if (num == sum):
