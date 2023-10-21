63204c131ad3f89e6700e6c598685d705b062b94
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2023-10-21 06:40:11 +0300

    added comments

--- 6 finding maximum of two numbers.py
+++ 6 finding maximum of two numbers.py
@@ -3,6 +3,7 @@
 number1 = input("Input the first number: ")
 number2 = input("Input the first number: ")
 
+#using max() python function
 max_num = max(float(number1), float(number2))
 
 print("The maximum number is ", int(max_num))
--- 7 check prime numbers.py
+++ 7 check prime numbers.py
@@ -4,10 +4,14 @@
 
 def primeNumbers(number):
 
+    # we first ensure the number is greater than 1
     if(int(number)>1):
+        # if the number is 2 or 3, then it is definitely a prime number
         if(number == 2 or number == 3):
             print(number, " is a prime number")
         else:
+            # if a number between 2 and half the number divides it and has a modulo of 0 then it is prime
+            # e.g if the number is 17 then range is (2,9) so if any number between 2 and 9 divides it perfectly then it is not a prime number
             for i in range (2, int(number/2)-1):
                 if (number%i) == 0:
                     print(number, " is not a prime number")
